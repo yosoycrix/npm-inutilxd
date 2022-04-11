b49afc383a7e1c1a400a1e9074fef37ae68fa8dd
yosoycrix <cristobalmogollon3012@gmail.com>
2022-04-11 13:37:02 +0000

    first commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const chalk = require('chalk');
+const app = express()
+
+const inutil = name => 'FUNCIONA!!!!!!!!!!! ' + name;
+
+module.exports = {
+  inutil,
+}
